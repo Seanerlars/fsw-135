dbad86267c8ac921525b487c3eed6120f75c325b
Seanerlars <Sean.larsen@bryanuniversity.edu>
2021-03-24 20:05:02 -0500

    pp1

--- week1/MS-DBSANDMONGOOSE/models/inventory.js
+++ week1/MS-DBSANDMONGOOSE/models/inventory.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const inventory = new Schema({
+const inventorySchema = new Schema({
     name: {
         type: String,
         required: true
@@ -17,4 +17,4 @@ const inventory = new Schema({
     }
 })
 
-module.exports = mongoose.inventory('inventory', inventory)
+module.exports = mongoose.model('inventory', inventorySchema)
